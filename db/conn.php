92718a552481aa28d563a159f07cdfb933e35964
drich2020 <60243531+drich2020@users.noreply.github.com>
2021-12-27 19:46:44 -0500

    Update conn.php

--- db/conn.php
+++ db/conn.php
@@ -13,6 +13,8 @@
     $pass = 'kYcex0mrjJdZNouDTZAl';
     $charset = 'utf8mb4';
 
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
     try{
         $pdo = new PDO($dsn, $user, $pass,);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
